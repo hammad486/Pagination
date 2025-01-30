1447aaf1c0c3a5316a29a237beaf4f606f365aee
Awais JMM <jmm.awaiskiyani@gmail.com>
2025-01-30 08:23:18 +0500

    created upload image

--- Pagination/Migrations/20250129104120_changeProduct.Designer.cs
+++ Pagination/Migrations/20250129104120_changeProduct.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pagination.Models;
 
@@ -11,9 +12,11 @@ using Pagination.Models;
 namespace Pagination.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250129104120_changeProduct")]
+    partial class changeProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
